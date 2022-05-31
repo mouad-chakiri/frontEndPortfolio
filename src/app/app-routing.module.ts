fe9459110ae932d600ba8f081f84c2dee949b94c
mouad chakiri <moad.chakiri@gmail.com>
2022-05-31 20:58:46 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogINComponent } from './pages/cours/log-in/log-in.component';
+import { SingUPComponent } from './pages/cours/sing-up/sing-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'' , component : LogINComponent 
+  } ,
+  {
+    path: 'singUp' , component : SingUPComponent
+  }
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogINComponent } from './pages/cours/log-in/log-in.component';
+import { SingUPComponent } from './pages/cours/sing-up/sing-up.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogINComponent,
+    SingUPComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
